ce29ce27e81d2bb9818c540ba20091c9e1131cee
David Salek <salekd@gmail.com>
2017-11-20 23:44:31 +0100

    initial commit

---
+++ function/handler.py
@@ -0,0 +1,97 @@
+import json
+from json import encoder
+import os
+import sys
+
+# Location of the pre-compiled dependencies
+sys.path.append(os.path.join("/models/research"))
+sys.path.append(os.path.join("/models/research/slim"))
+
+# Now that the script knows where to look, we can safely import our objects
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import base64
+from io import BytesIO
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+MODEL_NAME = '/ssd_mobilenet_v1_coco_11_06_2017'
+PATH_TO_CKPT = os.path.join(MODEL_NAME, 'frozen_inference_graph.pb')
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('/models/research/object_detection', 'data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+def detect_objects(image_np, sess, detection_graph):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Each box represents a part of the image where a particular object was detected.
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represent the level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Actual detection.
+    (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    return scores, classes, image_np
+
+
+def handle(req):  
+    json_req = json.loads(req)
+
+    # Load image
+    image_data = base64.b64decode(json_req['image'])
+    image = Image.open(BytesIO(image_data))
+    (im_width, im_height) = image.size
+    image_np = np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+    # Load a (frozen) Tensorflow model into memory.
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    # Detect objects
+    scores, classes, image_with_labels = detect_objects(image_np, sess, detection_graph)
+    #print("\n".join("{0:<20s}: {1:.1f}%".format(category_index[c]['name'], s*100.) for (c, s) in zip(classes[0], scores[0])))
+
+    sess.close()
+
+    encoder.FLOAT_REPR = lambda f: format(f, '.4f')
+    encoder.c_make_encoder = None
+    result = [{'class': category_index[c]['name'], 'score': float(s)} for (c, s) in zip(classes[0], scores[0])]
+    print(json.dumps(result))
