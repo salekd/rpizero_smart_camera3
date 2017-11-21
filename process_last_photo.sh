a4afe8796d1f536c8fd94928509ce597d42db572
David Salek <salekd@gmail.com>
2017-11-21 18:30:28 +0100

    script to process the last image taken by motion

---
+++ process_last_photo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export last_file=`ls /home/pi/motion -rt | tail -n 1`
+curl 192.168.2.2:8080/async-function/faas-mobilenet \
+    -d "$(echo -n '{"image": "'; base64 $last_file | tr -d '\n'; echo '"}')" \
+    -H "X-Callback-Url: 192.168.2.2:8080/function/faas-s3-email"
+rm $last_file
