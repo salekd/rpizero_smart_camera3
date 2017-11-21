0cc13e772f761c267368644bb5ea33c903db8385
David Salek <salekd@gmail.com>
2017-11-21 22:00:47 +0100

    correct url format

--- process_last_photo.sh
+++ process_last_photo.sh
@@ -2,5 +2,5 @@
 export last_file=`ls /home/pi/motion -rt | tail -n 1`
 curl 192.168.2.2:8080/async-function/faas-mobilenet \
     -d "$(echo -n '{"image": "'; base64 $last_file | tr -d '\n'; echo '"}')" \
-    -H "X-Callback-Url: 192.168.2.2:8080/function/faas-s3-email"
+    -H "X-Callback-Url: http://192.168.2.2:8080/function/faas-s3-email"
 rm $last_file
