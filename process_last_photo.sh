2b83b18d61b105736a8421b66d7b8fc3a90047d5
David Salek <salekd@gmail.com>
2017-11-21 23:15:33 +0100

    correction in the ls command

--- process_last_photo.sh
+++ process_last_photo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export last_file=`ls /home/pi/motion -rt | tail -n 1`
+export last_file=`ls -rt /home/pi/motion | tail -n 1`
 curl 192.168.2.2:8080/async-function/faas-mobilenet \
     -d "$(echo -n '{"image": "'; base64 $last_file | tr -d '\n'; echo '"}')" \
     -H "X-Callback-Url: http://192.168.2.2:8080/function/faas-s3-email"
