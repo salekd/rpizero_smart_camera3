0770e8b634d354551e42850dff84bf9636c15de9
David Salek <salekd@gmail.com>
2017-11-23 09:56:06 +0100

    rm -f

--- process_last_photo.sh
+++ process_last_photo.sh
@@ -3,4 +3,4 @@ export last_file=`ls -rt /home/pi/motion | tail -n 1`
 curl 192.168.2.2:8080/async-function/faas-mobilenet \
     -d "$(echo -n '{"image": "'; base64 $last_file | tr -d '\n'; echo '"}')" \
     -H "X-Callback-Url: http://192.168.2.2:8080/function/faas-s3-email"
-rm $last_file
+rm -f $last_file
