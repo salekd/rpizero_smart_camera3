523cbb0e13351138f99f3c372ca66bd024e3d3cb
David Salek <salekd@gmail.com>
2017-12-06 12:43:21 +0100

    extract filename without the full path

--- process_last_photo.sh
+++ process_last_photo.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export last_file=`ls -rt /home/pi/motion/*jpg | tail -n 1`
+export img_dir=/home/pi/motion
+export last_file=`(cd $img_dir; ls -rt *jpg | tail -n 1)`
 curl 192.168.2.2:8080/async-function/faas-mobilenet \
-    -d "$(echo -n '{"filename": "'; echo -n $last_file; echo -n '", "image_data": "'; base64 $last_file | tr -d '\n'; echo '"}')" \
+    -d "$(echo -n '{"filename": "'; echo -n $last_file; echo -n '", "image_data": "'; base64 $img_dir/$last_file | tr -d '\n'; echo '"}')" \
     -H "X-Callback-Url: http://192.168.2.2:8080/function/faas-s3-email"
-rm -f $last_file
+rm -f $img_dir/$last_file
